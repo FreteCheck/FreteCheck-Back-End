9f12673dc3fba0e68acaadc1b8e94ed3da12b66b
Vagner Ferreira Lima Junior <116043257+vagnerflj@users.noreply.github.com>
2025-05-10 12:48:48 -0300

    feat: Escopo do projeto Back end

---
+++ freteCheck/src/main/java/com/example/freteCheck/FreteCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.freteCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreteCheckApplication.class, args);
+	}
+
+}
