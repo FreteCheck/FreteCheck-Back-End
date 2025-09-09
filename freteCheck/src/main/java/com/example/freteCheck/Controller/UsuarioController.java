ed3a4c5147ce3eacf517ce5b19983ebde2509177
Vagner Ferreira Lima Junior <116043257+vagnerflj@users.noreply.github.com>
2025-09-08 22:26:54 -0300

    feat(auth): adiciona endpoint de login de usuário

--- freteCheck/src/main/java/com/example/freteCheck/Controller/UsuarioController.java
+++ freteCheck/src/main/java/com/example/freteCheck/Controller/UsuarioController.java
@@ -1,9 +1,13 @@
 package com.example.freteCheck.Controller;
 
+import com.example.freteCheck.Dto.LoginRequestDTO;
 import com.example.freteCheck.model.Usuario;
 import com.example.freteCheck.Service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -16,5 +20,15 @@ public class UsuarioController {
     @PostMapping
     public Usuario cadastrar(@RequestBody Usuario usuario) {
         return usuarioService.cadastrar(usuario);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequest) {
+        try {
+            Usuario usuarioAutenticado = usuarioService.autenticar(loginRequest);
+            return ResponseEntity.ok(Map.of("message", "Login bem-sucedido!"));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(401).body(Map.of("message", e.getMessage()));
+        }
     }
 }
